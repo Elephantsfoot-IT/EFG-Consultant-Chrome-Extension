b2fbe175589d907dd1e4fb97a3948352826c7554
khangtrinh1999 <khanggran3147@gmail.com>
2025-04-03 11:57:59 +1100

    full quote

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 // Select elements from the DOM
 const quoteIdInput = document.getElementById('quoteId');
 const generateButton = document.getElementById('generate');
+const generateFullButton = document.getElementById('generate-full');
 const resultElement = document.getElementById('result');
 const copyButton = document.getElementById('copy');
 
@@ -96,6 +97,69 @@ generateButton.addEventListener('click', async () => {
         showError(error)
         generateButton.innerHTML = originalContent;
         generateButton.disabled = false;
+    }
+});
+
+
+
+generateFullButton.addEventListener('click', async () => {
+    const quoteId = quoteIdInput.value.trim();
+    if (!quoteId) {
+        showError('Please enter a Quote ID')
+        return;
+    }
+    else{
+        hideError()
+    }
+    // Save original button content
+    const originalContent = generateFullButton.innerHTML;
+    // Set loading state with spinning icon
+    generateFullButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" 
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+            class="spin">
+            <path d="M21 12a9 9 0 1 1-6.219-8.56"/>
+        </svg>
+    `;
+    generateFullButton.disabled = true;
+
+
+    try {
+        // Send PUT request to your API endpoint with the scoro_quote_id
+        const response = await fetch('https://elephantsfoot-consultant-online-quote-form.vercel.app/api/create-full-form', {
+            method: 'PUT',
+            mode: 'cors',  // Add this line
+            credentials: 'omit',  // Add this line
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'  // Add this line
+            },
+            body: JSON.stringify({
+                scoro_quote_id: quoteId,
+            }),
+        });
+
+        // Check if the request was successful
+        if (!response.ok) {
+            const data = await response.json();
+            throw new Error(data.error);
+        }
+
+        // Parse the response
+        const data = await response.json();
+        const tokenUrl = data.url ? data.url : ''; // Assuming the response contains the token's ID
+
+        // Display the result (token URL or token ID)
+        resultElement.value = tokenUrl;
+
+        generateFullButton.innerHTML = originalContent;
+        generateFullButton.disabled = false;
+        hideError()
+
+    } catch (error) {
+        showError(error)
+        generateFullButton.innerHTML = originalContent;
+        generateFullButton.disabled = false;
     }
 });
 
